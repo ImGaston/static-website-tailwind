268dbc2ea298f246a788a7843075444046216eee
Gaston <corbalan.gaston@outlook.com>
2022-11-18 17:45:15 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <Header />
       <Hero />
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1 className='text-3xl font-bold'>CM Charlone</h1>
     </>
   );
 }
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,107 +1,88 @@
 import { useState } from 'react';
 import logo from '../assets/logo-mobile.png';
+import { HiBars3, HiOutlineXMark } from 'react-icons/hi2';
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <div className='flex items-center justify-between bg-primary p-4'>
-      <img
-        src={logo}
-        alt='Logo CM Charlone'
-        className='w-44'
-      />
-      <nav>
-        <section className='MOBILE-MENU flex lg:hidden'>
-          <div
-            className='HAMBURGER-ICON space-y-2'
-            onClick={() => setIsNavOpen((prev) => !prev)}
+    <nav className='relative mb-3 flex flex-wrap items-center justify-between bg-primary px-2 py-3'>
+      <div className='container mx-auto flex flex-wrap items-center justify-between px-4'>
+        <div className='relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start'>
+          <img
+            src={logo}
+            alt='Logo CM Charlone'
+            className='w-44'
+          />
+          <button
+            className='block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-2xl leading-none text-white outline-none focus:outline-none lg:hidden'
+            type='button'
+            onClick={() => setNavbarOpen(!navbarOpen)}
           >
-            <span className='block h-0.5 w-8 animate-pulse bg-white'></span>
-            <span className='block h-0.5 w-8 animate-pulse bg-white'></span>
-            <span className='block h-0.5 w-8 animate-pulse bg-white'></span>
-          </div>
-
-          <div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
-            <div
-              className='absolute top-0 right-0 px-8 py-8'
-              onClick={() => setIsNavOpen(false)}
-            >
-              <svg
-                className='h-8 w-8 text-gray-600'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
+            {navbarOpen ? <HiOutlineXMark /> : <HiBars3 />}
+          </button>
+        </div>
+        <div
+          className={
+            'flex-grow items-center lg:flex' +
+            (navbarOpen ? ' flex' : ' hidden')
+          }
+          id='example-navbar-danger'
+        >
+          <ul className='flex list-none flex-col lg:ml-auto lg:flex-row'>
+            <li className='nav-item'>
+              <a
+                className='mt-4 flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                href='#'
               >
-                <line
-                  x1='18'
-                  y1='6'
-                  x2='6'
-                  y2='18'
-                />
-                <line
-                  x1='6'
-                  y1='6'
-                  x2='18'
-                  y2='18'
-                />
-              </svg>
-            </div>
-            <ul className='flex min-h-[250px] flex-col items-center justify-between'>
-              <li className='my-4 border-b border-gray-400 uppercase'>
-                <a href='/about'>Inicio</a>
-              </li>
-              <li className='my-4 border-b border-gray-400 uppercase'>
-                <a href='/portfolio'>Nosotros</a>
-              </li>
-              <li className='my-4 border-b border-gray-400 uppercase'>
-                <a href='/contact'>Servicios</a>
-              </li>
-              <li className='my-4 border-b border-gray-400 uppercase'>
-                <a href='/contact'>Profesionales</a>
-              </li>
-              <li className='my-4 border-b border-gray-400 uppercase'>
-                <a href='/contact'>Novedades</a>
-              </li>
-              <li className='my-4 border-b border-gray-400 uppercase'>
-                <a href='/contact'>Contacto</a>
-              </li>
-            </ul>
-          </div>
-        </section>
-        {/* <ol className='ml-4 pb-2'>
-          <li className='text-white'>Inicio</li>
-          <li className='text-white'>Nosotros</li>
-          <li className='text-white'>Servicios</li>
-          <li className='text-white'>Profesionales</li>
-          <li className='text-white'>Novedades</li>
-          <li className='text-white'>Contacto</li>
-          <li className='text-white'>TURNOS</li>
-        </ol> */}
-      </nav>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-    </div>
+                <span className='ml-2'>Inicio</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                href='#'
+              >
+                <span className='ml-2'>Nosotros</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                href='#'
+              >
+                <span className='ml-2'>Servicios</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                href='#'
+              >
+                <span className='ml-2'>Profesionales</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                href='#'
+              >
+                <span className='ml-2'>Novedades</span>
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a
+                className='flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                href='#'
+              >
+                <span className='ml-2'>Contacto</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
