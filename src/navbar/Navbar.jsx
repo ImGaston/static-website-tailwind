292816da4d82cd1c6e7ec7d5702ed508e0710d1f
Gaston <corbalan.gaston@outlook.com>
2022-11-18 19:25:16 -0300

    update navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,11 +3,10 @@ import logo from '../assets/logo-mobile.png';
 import { HiBars3, HiOutlineXMark } from 'react-icons/hi2';
 
 const Navbar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className='relative mb-3 flex flex-wrap items-center justify-between bg-primary px-2 py-3'>
+    <nav className=' mb-3 flex flex-wrap items-center justify-between bg-primary px-2 py-3'>
       <div className='container mx-auto flex flex-wrap items-center justify-between px-4'>
         <div className='relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start'>
           <img
@@ -16,7 +15,7 @@ const Navbar = () => {
             className='w-44'
           />
           <button
-            className='block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-2xl leading-none text-white outline-none focus:outline-none lg:hidden'
+            className='block cursor-pointer rounded px-3 py-1 text-2xl leading-none text-white outline-none focus:outline-none lg:hidden'
             type='button'
             onClick={() => setNavbarOpen(!navbarOpen)}
           >
@@ -28,12 +27,11 @@ const Navbar = () => {
             'flex-grow items-center lg:flex' +
             (navbarOpen ? ' flex' : ' hidden')
           }
-          id='example-navbar-danger'
         >
           <ul className='flex list-none flex-col lg:ml-auto lg:flex-row'>
             <li className='nav-item'>
               <a
-                className='mt-4 flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
+                className='flex items-center px-3 py-2 leading-snug text-white hover:opacity-75'
                 href='#'
               >
                 <span className='ml-2'>Inicio</span>
